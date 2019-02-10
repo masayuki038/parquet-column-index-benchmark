45e5171789078a57e29fd8e4116760fc7f099df8
Masayuki Takahashi <masayuki038@gmail.com>
2019-02-10 11:31:58 +0900

    Fix the target selection

--- src/main/java/net/wrap_trap/parquet_column_index_bench/Bench.java
+++ src/main/java/net/wrap_trap/parquet_column_index_bench/Bench.java
@@ -22,6 +22,8 @@ import static org.apache.parquet.filter2.predicate.FilterApi.eq;
 
 public class Bench {
 
+  private static final String TARGET = "abcdefghijklmnopqrstuvwxyz5242880";
+
   @Benchmark
   public void filteringScanWithColumnIndex() throws IOException {
     filteringScan(true);
@@ -34,13 +36,14 @@ public class Bench {
 
   private void filteringScan(boolean useColumnIndexFilter) throws IOException {
     Path inPath = new Path(Generator.TEST_FILE);
-    FilterPredicate actualFilter = eq(binaryColumn("binary_field"), Binary.fromString("abcdefghijklmnopqrstuvwxyz524288"));
+    FilterPredicate actualFilter = eq(binaryColumn("binary_field"), Binary.fromString(TARGET));
     ParquetReader<Group> reader = ParquetReader.builder(new GroupReadSupport(), inPath)
       .withFilter(FilterCompat.get(actualFilter))
       .useColumnIndexFilter(useColumnIndexFilter).build();
     Group group;
     while ((group = reader.read()) != null) {
-      assert(group.getBinary("binary_field", 0).toString().equals("abcdefghijklmnopqrstuvwxyz524288"));
+      System.out.println(TARGET + " Found");
+      assert(group.getBinary("binary_field", 0).toString().equals(TARGET));
     }
   }
 
